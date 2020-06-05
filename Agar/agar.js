fb748ee967338c58006f3452de1fba43882a2109
Arash Ghabussi <arashghabussi@gmail.com>
2020-06-05 11:08:59 -0700

    eating v1

--- Agar/agar.js
+++ Agar/agar.js
@@ -9,10 +9,15 @@ let foodFactory = [];
 var windowWidth = screen.width;
 var windowHeight = screen.height;
 speed = 100;
-foodMax = 10;
-
+foodMax = 100;
+// Screen Size
 console.log(`windowWidth ${windowWidth} windowHeight ${windowHeight} `);
-   
+
+// X,Y Values
+function getCssProperty(elmId, property){
+    var elem = document.getElementById(elmId);
+    return window.getComputedStyle(elem,null).getPropertyValue(property);
+ }
 
 function foodGenerator(){    
         setTimeout(function() {
@@ -22,6 +27,7 @@ function foodGenerator(){
             
             //food detail:
             var foodAs = document.createElement("div-food"); 
+            //foodAs.setAttribute("id", "id-food");
             foodFactory.push(foodAs);
             foodNumber ++;
 
@@ -37,28 +43,31 @@ function foodGenerator(){
 
 function eating(){
 
-    player.width = 50;
-    player.height = 50;
+    //player.width = "90px";
+    //player.height = "90px";
 
+    //var foodX = getCssProperty("id-food", "left");
+    //console.log(`value of food not in px is ${parseInt(foodX)}`);
+
+    //console.log(parseInt(player.style.left));
 
     for(let i = 0; i < foodFactory.length; i++){
 
-        foodFactory[i].width = "40px";
-        foodFactory[i].height = "40px";
+        //foodFactory[i].width = "30px";
+        //foodFactory[i].height = "30px";
 
-        console.log(`Player => x: ${player.style.left} y: ${player.style.top} width: ${player.width} height: ${player.height}`);
-        console.log(`Food => x: ${foodFactory[i].style.left} y: ${foodFactory[i].style.top} width: ${foodFactory[i].width} height: ${foodFactory[i].height}`);
-        console.log(`( (player.style.left) ${player.style.left} < (foodFactory[i].style.left + foodFactory[i].width) ${foodFactory[i].style.left + foodFactory[i].width})`);
+        //console.log(`Player => x: ${player.style.left} y: ${player.style.top} width: ${player.width} height: ${player.height}`);
+        //console.log(`Food => x: ${foodFactory[i].style.left} y: ${foodFactory[i].style.top} width: ${foodFactory[i].width} height: ${foodFactory[i].height}`);
+        //console.log(`( (player.style.left) ${player.style.left} < (foodFactory[i].style.left + foodFactory[i].width) ${foodFactory[i].style.left + 30 + 'px'})`);
 
-        if ( ( (player.style.left) < (foodFactory[i].style.left + foodFactory[i].width) ) &&  
-             ( (player.style.left + player.width)  > (foodFactory[i].style.left) ) &&
-             ( (player.style.top) < (foodFactory[i].style.top + foodFactory[i].height) ) && 
-             ( (player.style.top + player.height) > (foodFactory[i].style.top) ) 
+        if ( ( (parseInt(player.style.left)) < (parseInt(foodFactory[i].style.left) + 30) )   &&  
+             ( (parseInt(player.style.left) + 90)  > (parseInt(foodFactory[i].style.left)) )   &&
+             ( (parseInt(player.style.top)) < (parseInt(foodFactory[i].style.top) + 30) )     && 
+             ( (parseInt(player.style.top) + 90) > (parseInt(foodFactory[i].style.top)) )
         ){
             console.log(`collid`);
             foodFactory[i].remove(); 
         }
-        console.log(`food I ${foodFactory[i]} I: ${i}`);
     }
 }
 
@@ -69,7 +78,6 @@ function follow(e){
     player.style.left = (horizontalPositionPlayer + 'px');
     player.style.top = (verticalPositionPlayer + 'px');
     //player position
-
 }
 
 function brain(e){
@@ -79,10 +87,13 @@ function brain(e){
 }
 
 AREA.addEventListener('mousemove', brain);
-
 setInterval(foodGenerator, 1000);
 
 
 
-//(player.style.left < foodFactory[i].style.left + foodFactory[i].width  && player.style.left + player.width  > foodFactory[i].style.left) &&
-//(player.style.top < foodFactory[i].style.top + foodFactory[i].height && player.style.top + player.height > foodFactory[i].style.top) ){
+/**
+ * ( (player.style.left) < (foodFactory[i].style.left + foodFactory[i].width) ) &&  
+             ( (player.style.left + player.width)  > (foodFactory[i].style.left) ) &&
+             ( (player.style.top) < (foodFactory[i].style.top + foodFactory[i].height) ) && 
+             ( (player.style.top + player.height) > (foodFactory[i].style.top) ) 
+ */
