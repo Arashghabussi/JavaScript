1b49000a25281a7868f5f8d91cff6d0d5c998146
Arash Ghabussi <arashghabussi@gmail.com>
2020-06-05 11:10:01 -0700

    Eating Done

--- Agar/agar.js
+++ Agar/agar.js
@@ -13,12 +13,6 @@ foodMax = 100;
 // Screen Size
 console.log(`windowWidth ${windowWidth} windowHeight ${windowHeight} `);
 
-// X,Y Values
-function getCssProperty(elmId, property){
-    var elem = document.getElementById(elmId);
-    return window.getComputedStyle(elem,null).getPropertyValue(property);
- }
-
 function foodGenerator(){    
         setTimeout(function() {
         if(foodMax > 0){
@@ -27,7 +21,6 @@ function foodGenerator(){
             
             //food detail:
             var foodAs = document.createElement("div-food"); 
-            //foodAs.setAttribute("id", "id-food");
             foodFactory.push(foodAs);
             foodNumber ++;
 
@@ -35,7 +28,6 @@ function foodGenerator(){
             foodAs.style.top = (y + 'px');
 
             map.appendChild(foodAs);
-
             foodMax --;
         }
     }, 1000);
@@ -43,23 +35,11 @@ function foodGenerator(){
 
 function eating(){
 
-    //player.width = "90px";
-    //player.height = "90px";
-
-    //var foodX = getCssProperty("id-food", "left");
-    //console.log(`value of food not in px is ${parseInt(foodX)}`);
-
-    //console.log(parseInt(player.style.left));
-
     for(let i = 0; i < foodFactory.length; i++){
-
-        //foodFactory[i].width = "30px";
-        //foodFactory[i].height = "30px";
 
         //console.log(`Player => x: ${player.style.left} y: ${player.style.top} width: ${player.width} height: ${player.height}`);
         //console.log(`Food => x: ${foodFactory[i].style.left} y: ${foodFactory[i].style.top} width: ${foodFactory[i].width} height: ${foodFactory[i].height}`);
-        //console.log(`( (player.style.left) ${player.style.left} < (foodFactory[i].style.left + foodFactory[i].width) ${foodFactory[i].style.left + 30 + 'px'})`);
-
+       
         if ( ( (parseInt(player.style.left)) < (parseInt(foodFactory[i].style.left) + 30) )   &&  
              ( (parseInt(player.style.left) + 90)  > (parseInt(foodFactory[i].style.left)) )   &&
              ( (parseInt(player.style.top)) < (parseInt(foodFactory[i].style.top) + 30) )     && 
@@ -74,10 +54,9 @@ function eating(){
 function follow(e){
     var horizontalPositionPlayer = e.clientX;
     var verticalPositionPlayer = e.clientY;
-
+    //player position
     player.style.left = (horizontalPositionPlayer + 'px');
     player.style.top = (verticalPositionPlayer + 'px');
-    //player position
 }
 
 function brain(e){
@@ -88,12 +67,3 @@ function brain(e){
 
 AREA.addEventListener('mousemove', brain);
 setInterval(foodGenerator, 1000);
-
-
-
-/**
- * ( (player.style.left) < (foodFactory[i].style.left + foodFactory[i].width) ) &&  
-             ( (player.style.left + player.width)  > (foodFactory[i].style.left) ) &&
-             ( (player.style.top) < (foodFactory[i].style.top + foodFactory[i].height) ) && 
-             ( (player.style.top + player.height) > (foodFactory[i].style.top) ) 
- */
